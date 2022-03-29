9190e67dadd5deb47680ab24d4582640f44d8208
yubin <yubin@itdadev.com>
2022-03-29 11:58:58 +0900

    styling exercise 1158

--- components/Styling.js
+++ components/Styling.js
@@ -1,7 +1,39 @@
 import { StyleSheet, View, Text } from "react-native";
 
 const Styling = () => {
-  return <View>Styling</View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.square}>
+        <Text>Square 1</Text>
+      </View>
+      <View style={styles.square}>
+        <Text>Square 2</Text>
+      </View>
+      <View style={styles.square}>
+        <Text>Square 3</Text>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  square: {
+    backgroundColor: "aquamarine",
+    width: 100,
+    height: 100,
+    margin: 10,
+    textAlign: "center",
+    justifyContent: "center",
+  },
+
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    top: "50%",
+    position: "absolute",
+    transform: "translateY(-50%)",
+  },
+});
 
 export default Styling;
